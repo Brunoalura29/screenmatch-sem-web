a60cd0700cb0e488d99898db4d019a6541ed253e
Bruno Henrique <bruno_hdias@hotmail.com>
2024-09-23 21:30:57 -0300

    Commit final

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -7,12 +7,13 @@ import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class Principal{
+public class Principal {
 
 
     private Scanner leitura = new Scanner(System.in);
@@ -24,7 +25,7 @@ public class Principal{
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&2&apikey=6585022c";
 
-    public void exibeMenu(){
+    public void exibeMenu() {
         System.out.println("Digite o nome da serie para busca");
         var nomeSerie = leitura.nextLine();
         var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
@@ -33,38 +34,43 @@ public class Principal{
 
         List<DadosTemporada> temporadas = new ArrayList<>();
 
-		for (int i = 1; i <= dados.totalTemporadas(); i++){
-			json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-		temporadas.forEach(System.out::println);
+        for (int i = 1; i <= dados.totalTemporadas(); i++) {
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
 
-//        for(int i = 0; i < dados.totalTemporadas(); i++){
-//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-//            for(int j = 0; j< episodiosTemporada.size(); j++){
-//                System.out.println(episodiosTemporada.get(j).titulo());
-//            }
-//        }
+        for(int i = 0; i < dados.totalTemporadas(); i++){
+            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+            for(int j = 0; j< episodiosTemporada.size(); j++){
+                System.out.println(episodiosTemporada.get(j).titulo());
+            }
+        }
 
-       temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
 
-//        List<String> nomes = Arrays.asList("Jaque", "Iasmin", "Paulo", "Rodrigo", "Nico");
-//
-//        nomes.stream()
-//                .sorted()
-//                .limit(3)
-//                .forEach(System.out::println);
+        List<String> nomes = Arrays.asList("Jaque", "Iasmin", "Paulo", "Rodrigo", "Nico");
+
+        nomes.stream()
+                .sorted()
+                .limit(3)
+                .forEach(System.out::println);
 
         List<DadosEpisodio> dadosEpisodios = temporadas.stream()
                 .flatMap(t -> t.episodios().stream())
                 .collect(Collectors.toUnmodifiableList());
 
-        System.out.println("\n Top 5 epsodios");
+        System.out.println("\n Top 10 epsodios");
         dadosEpisodios.stream()
                 .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .peek(e -> System.out.println("Primeiro filtro(N/A) " + e))
                 .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
-                .limit(5)
+                .peek(e -> System.out.println("Ordenação " + e))
+                .limit(10)
+                .peek(e -> System.out.println("Limite " + e))
+                .map(e -> e.titulo().toUpperCase())
+                .peek(e -> System.out.println("Mapeamento " + e))
                 .forEach(System.out::println);
 
         List<Episodio> episodios = temporadas.stream()
@@ -73,6 +79,18 @@ public class Principal{
                 ).collect(Collectors.toList());
 
         episodios.forEach(System.out::println);
+
+        System.out.println("Digite um trecho do titulo do episódio");
+        var trechoTitulo = leitura.nextLine();
+        Optional<Episodio> epsodioBuscado = episodios.stream()
+                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+                .findFirst();
+        if(epsodioBuscado.isPresent()) {
+            System.out.println("Episodio encontrado!");
+            System.out.println("Temporada: " + epsodioBuscado.get().getTemporada());
+        } else {
+            System.out.println("Episodio não encontrado");
+        }
 
         System.out.println("A partir de que ano você deseja ver os epsodios? ");
         var ano = leitura.nextInt();
@@ -89,5 +107,27 @@ public class Principal{
                                 "Episodio: " + e.getTitulo() +
                                 "Data lançamento " + e.getDataLancamento().format(formatador)
                 ));
+
+
+        Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada,
+                        Collectors.averagingDouble(Episodio::getAvaliacao)));
+      // System.out.println(avaliacoesPorTemporada);
+
+        avaliacoesPorTemporada.forEach((temporada, avaliacao) ->
+                System.out.println("Temporada " + temporada + ": " + String.format("%.2f", avaliacao)));
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+       // System.out.println(est);
+
+        System.out.println(String.format("Count: %d, Sum: %.2f, Min: %.4f, Average: %.2f, Max: %.2f",
+                est.getCount(), est.getSum(), est.getMin(), est.getAverage(), est.getMax()));
+
     }
 }
+
+
+
